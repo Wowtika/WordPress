622a16a2b53695b4a413074a7adb09eede33433f
Konstantin Baranchuk <kos5anchik@gmail.com>
2024-10-12 17:53:10 +0300

    feat: market links and rating uploading (issues#20, #23)

--- friction-master/template-product.php
+++ friction-master/template-product.php
@@ -428,6 +428,20 @@ get_header() ?>
 						<div class="footer-card__heading">
 							<?php the_field('About this part','option');?>
 						</div>
+						
+						<?php
+							wp_reset_postdata();
+							$query->rewind_posts();
+							if ( $query->have_posts() ) { 
+								while ( $query->have_posts() ) { 
+									$query->the_post(); 
+									$title = get_the_title();
+									if (stripos($title, $type) !== false && stripos($title, $group) !== false) {
+										break;
+									};
+								} 
+							}
+						?>
 
 						<?php the_content(); ?>
 
@@ -446,14 +460,62 @@ get_header() ?>
 					</div>
 
 					<div class="markets-card__logos">
-						<a href="#" class="markets-card__logo">
-							<img src="<?=get_template_directory_uri();?>/assets/img/gallery/ebay.jpg" alt="">
-						</a>
-						<a href="#" class="markets-card__logo">
-							<img src="<?=get_template_directory_uri();?>/assets/img/gallery/wallmart.jpg" alt="">
-						</a>
-					</div>
+					<!-- <?php
+						$url = 'https://catalog.loopautomotive.com/catalog/part-attributes?part_id=' . $_GET['part_id']; // Замените на фактический URL вашего API
 
+						// Заголовки запроса
+						$headers = array(
+							'Content-Type: application/json',
+						);
+					
+						// Создаем новый cURL ресурс
+						$ch = curl_init($url);
+					
+						// Устанавливаем опции cURL
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+						//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params)); // Преобразуем параметры в JSON и передаем их в теле запроса
+					
+						// Выполняем запрос
+						$response = curl_exec($ch);
+					
+						// Проверяем наличие ошибок
+						if (curl_errno($ch)) {
+							echo 'Ошибка cURL: ' . curl_error($ch);
+						} else {
+							$data = json_decode($response, true);
+							
+							// Печатаем данные
+							//print_r($data);
+							$partAttribute = $data;
+							//var_dump($data);
+						}
+						curl_close($ch);
+					?> -->
+					<!-- api -->
+						<?php 
+							wp_reset_postdata();
+							if (have_rows('online_retailers')) {
+								while (have_rows('online_retailers')) {
+									the_row();
+									$linkToMarket = "https://www.amazon.com/dp/B000CHB5SM"; //Получить из api
+									echo '<a href="' . $linkToMarket . '" class="markets-card__logo">';
+										echo '<img src="' . get_sub_field('logo') . '">';
+									echo '</a>';
+								}
+							}
+							$query->rewind_posts();
+							if ( $query->have_posts() ) { 
+								while ( $query->have_posts() ) { 
+									$query->the_post(); 
+									$title = get_the_title();
+									if (stripos($title, $type) !== false && stripos($title, $group) !== false) {
+										break;
+									};
+								} 
+							}
+						?>
+					</div>
 					<div class="markets-card__footer">
 
 						<a href="#" class="markets-card__link"><?php the_field('more_buying_options','option');?></a>
@@ -562,11 +624,7 @@ get_header() ?>
 
 	<?php
 		wp_reset_postdata();
-		$query = new WP_Query( array(
-			'post_type' => 'product-lines',
-			'order' => 'ASC',
-			'orderby' => 'ID',
-		));
+		$query->rewind_posts();
 		if ( $query->have_posts() ) { 
 			while ( $query->have_posts() ) { 
 				$query->the_post(); 
@@ -596,12 +654,56 @@ get_header() ?>
 					<!-- <img src="<?=get_template_directory_uri();?>/assets/img/home/arrow-left.png" alt="" class="partners-left-arrow__img" /> -->
 				</span>
 				<div class="swiper partners-slider">
+					
+					<?php wp_reset_postdata(); ?>
+
 					<div class="swiper-wrapper partners-slider__wrapper ">
 
-						<!-- <div class="swiper-slide partners-slider__card ">
-							<a href="#" class="partners-slider__link">
-								<img src="<?=get_template_directory_uri();?>/assets/img/partners/part1.svg" alt="" class="partners-slider__image">
+					<?php if (have_rows('online_retailers')) { ?>
+					<?php while (have_rows('online_retailers')) { the_row();?>
+					<!-- <?php
+						$url = 'https://catalog.loopautomotive.com/catalog/part-attributes?part_id=' . $_GET['part_id']; // Замените на фактический URL вашего API
+
+						// Заголовки запроса
+						$headers = array(
+							'Content-Type: application/json',
+						);
+					
+						// Создаем новый cURL ресурс
+						$ch = curl_init($url);
+					
+						// Устанавливаем опции cURL
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+						//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params)); // Преобразуем параметры в JSON и передаем их в теле запроса
+					
+						// Выполняем запрос
+						$response = curl_exec($ch);
+					
+						// Проверяем наличие ошибок
+						if (curl_errno($ch)) {
+							echo 'Ошибка cURL: ' . curl_error($ch);
+						} else {
+							$data = json_decode($response, true);
+							
+							// Печатаем данные
+							//print_r($data);
+							$partAttribute = $data;
+							//var_dump($data);
+						}
+						curl_close($ch);
+					?> -->
+					<!-- api -->
+					<?php $linkToMarket = "https://www.ebay.com/itm/116321581739"; //Получить из api ?>
+					<?php $ratingOnMarket = 4.8; //Получить из api ?>
+
+
+
+						<div class="swiper-slide partners-slider__card ">
+							<a href="<?=$linkToMarket ?>" class="partners-slider__link">
+								<img src='<?php echo get_sub_field('logo') ?>' alt="" class="partners-slider__image">
 							</a>
+
 							<div class="partners-slider__star">
 
 								<div class="rating rating_set">
@@ -615,148 +717,30 @@ get_header() ?>
 											<input type="radio" class="rating__item" value="5" name="rating">
 										</div>
 									</div>
-									<div class="rating__value"><b>3.6</b> / 5</div>
+									<div class="rating__value"><b><?= $ratingOnMarket ?></b> / 5</div>
 								</div>
 
 							</div>
-						</div> -->
-						<div class="swiper-slide partners-slider__card ">
-							<a href="#" class="partners-slider__link">
-								<img src="<?=get_template_directory_uri();?>/assets/img/partners/part2.svg" alt="" class="partners-slider__image">
-							</a>
-							<!-- <div class="partners-slider__star">
-								<div class="rating rating_set">
-									<div class="rating__body">
-										<div class="rating__active" style="width:66%"></div>
-										<div class="rating__items">
-											<input type="radio" class="rating__item" value="1" name="rating">
-											<input type="radio" class="rating__item" value="2" name="rating">
-											<input type="radio" class="rating__item" value="3" name="rating">
-											<input type="radio" class="rating__item" value="4" name="rating">
-											<input type="radio" class="rating__item" value="5" name="rating">
-										</div>
-									</div>
-									<div class="rating__value"><b>3.6</b> / 5</div>
-								</div>
-							</div> -->
-						</div>
-						<div class="swiper-slide partners-slider__card ">
-							<a href="#" class="partners-slider__link">
-								<img src="<?=get_template_directory_uri();?>/assets/img/partners/part3.svg" alt="" class="partners-slider__image">
-							</a>
-							<!-- <div class="rating rating_set">
-								<div class="rating__body">
-									<div class="rating__active" style="width:100%"></div>
-									<div class="rating__items">
-										<input type="radio" class="rating__item" value="1" name="rating">
-										<input type="radio" class="rating__item" value="2" name="rating">
-										<input type="radio" class="rating__item" value="3" name="rating">
-										<input type="radio" class="rating__item" value="4" name="rating">
-										<input type="radio" class="rating__item" value="5" name="rating">
-									</div>
-								</div>
-								<div class="rating__value"><b>5</b> / 5</div>
-							</div> -->
-						</div>
-						<div class="swiper-slide partners-slider__card ">
-							<a href="#" class="partners-slider__link">
-								<img src="<?=get_template_directory_uri();?>/assets/img/partners/part4.svg" alt="" class="partners-slider__image">
-							</a>
-							<!-- <div class="rating rating_set">
-								<div class="rating__body">
-									<div class="rating__active" style="width:20%"></div>
-									<div class="rating__items">
-										<input type="radio" class="rating__item" value="1" name="rating">
-										<input type="radio" class="rating__item" value="2" name="rating">
-										<input type="radio" class="rating__item" value="3" name="rating">
-										<input type="radio" class="rating__item" value="4" name="rating">
-										<input type="radio" class="rating__item" value="5" name="rating">
-									</div>
-								</div>
-								<div class="rating__value"><b>2</b> / 5</div>
-							</div> -->
 						</div>
 
-						<!-- <div class="swiper-slide partners-slider__card ">
-							<a href="#" class="partners-slider__link">
-								<img src="<?=get_template_directory_uri();?>/assets/img/partners/part1.svg" alt="" class="partners-slider__image">
-							</a>
-							<div class="partners-slider__star">
 
-								<div class="rating rating_set">
-									<div class="rating__body">
-										<div class="rating__active" style="width:66%"></div>
-										<div class="rating__items">
-											<input type="radio" class="rating__item" value="1" name="rating">
-											<input type="radio" class="rating__item" value="2" name="rating">
-											<input type="radio" class="rating__item" value="3" name="rating">
-											<input type="radio" class="rating__item" value="4" name="rating">
-											<input type="radio" class="rating__item" value="5" name="rating">
-										</div>
-									</div>
-									<div class="rating__value"><b>3.6</b> / 5</div>
-								</div>
-
-							</div>
-						</div> -->
-						<div class="swiper-slide partners-slider__card ">
-							<a href="#" class="partners-slider__link">
-								<img src="<?=get_template_directory_uri();?>/assets/img/partners/part2.svg" alt="" class="partners-slider__image">
-							</a>
-							<!-- <div class="partners-slider__star">
-								<div class="rating rating_set">
-									<div class="rating__body">
-										<div class="rating__active" style="width:66%"></div>
-										<div class="rating__items">
-											<input type="radio" class="rating__item" value="1" name="rating">
-											<input type="radio" class="rating__item" value="2" name="rating">
-											<input type="radio" class="rating__item" value="3" name="rating">
-											<input type="radio" class="rating__item" value="4" name="rating">
-											<input type="radio" class="rating__item" value="5" name="rating">
-										</div>
-									</div>
-									<div class="rating__value"><b>3.6</b> / 5</div>
-								</div>
-							</div> -->
-						</div>
-						<div class="swiper-slide partners-slider__card ">
-							<a href="#" class="partners-slider__link">
-								<img src="<?=get_template_directory_uri();?>/assets/img/partners/part3.svg" alt="" class="partners-slider__image">
-							</a>
-							<!-- <div class="rating rating_set">
-								<div class="rating__body">
-									<div class="rating__active" style="width:100%"></div>
-									<div class="rating__items">
-										<input type="radio" class="rating__item" value="1" name="rating">
-										<input type="radio" class="rating__item" value="2" name="rating">
-										<input type="radio" class="rating__item" value="3" name="rating">
-										<input type="radio" class="rating__item" value="4" name="rating">
-										<input type="radio" class="rating__item" value="5" name="rating">
-									</div>
-								</div>
-								<div class="rating__value"><b>5</b> / 5</div>
-							</div> -->
-						</div>
-						<div class="swiper-slide partners-slider__card ">
-							<a href="#" class="partners-slider__link">
-								<img src="<?=get_template_directory_uri();?>/assets/img/partners/part4.svg" alt="" class="partners-slider__image">
-							</a>
-							<!-- <div class="rating rating_set">
-								<div class="rating__body">
-									<div class="rating__active" style="width:20%"></div>
-									<div class="rating__items">
-										<input type="radio" class="rating__item" value="1" name="rating">
-										<input type="radio" class="rating__item" value="2" name="rating">
-										<input type="radio" class="rating__item" value="3" name="rating">
-										<input type="radio" class="rating__item" value="4" name="rating">
-										<input type="radio" class="rating__item" value="5" name="rating">
-									</div>
-								</div>
-								<div class="rating__value"><b>2</b> / 5</div>
-							</div> -->
-						</div>
-
+					<?php }} ?>
+					
 					</div>
+
+					<?php
+						$query->rewind_posts();
+						if ( $query->have_posts() ) { 
+							while ( $query->have_posts() ) { 
+								$query->the_post(); 
+								$title = get_the_title();
+								if (stripos($title, $type) !== false && stripos($title, $group) !== false) {
+									break;
+								};
+							} 
+						}
+					?>
+
 				</div>
 				<span class="slider-right-arrow partners-right _icon-arrow-new">
 					<!-- <img src="<?=get_template_directory_uri();?>/assets/img/home/arrow-left.png" alt="" class="partners-right-arrow__img" /> -->
@@ -771,11 +755,7 @@ get_header() ?>
 		<?php wp_reset_postdata() ?>
 		<?php get_template_part('template_part/how_it_works') ?>
 		<?php
-			$query = new WP_Query( array(
-				'post_type' => 'product-lines',
-				'order' => 'ASC',
-				'orderby' => 'ID',
-			));
+			$query->rewind_posts();
 			if ( $query->have_posts() ) { 
 				$counter = 0;
 				while ( $query->have_posts() ) { 
@@ -804,106 +784,6 @@ get_header() ?>
 				</div>
 
 				<div class="catalog__row">
-
-					<div class="catalog__item item-catalog">
-
-						<div class="item-catalog__header">
-							<div class="item-catalog__header-icons">
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card1.svg" alt=""></a>
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card2.svg" alt=""></a>
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card3.svg" alt=""></a>
-							</div>
-							<div class="item-catalog__header-heading">
-								R1915
-							</div>
-						</div>
-
-						<div class="item-catalog__image">
-							<img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-item1.jpg" alt="Sxema">
-						</div>
-
-						<div class="item-catalog__footer">
-							<a href="#">Show more</a>
-							<button type="submit" class="item-catalog__footer-button buy-button">BUY</button>
-
-						</div>
-
-					</div>
-
-					<div class="catalog__item item-catalog">
-
-						<div class="item-catalog__header">
-							<div class="item-catalog__header-icons">
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card1.svg" alt=""></a>
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card2.svg" alt=""></a>
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card3.svg" alt=""></a>
-							</div>
-							<div class="item-catalog__header-heading">
-								R1915
-							</div>
-						</div>
-
-						<div class="item-catalog__image">
-							<img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-item1.jpg" alt="Sxema">
-						</div>
-
-						<div class="item-catalog__footer">
-							<a href="#">Show more</a>
-							<button type="submit" class="item-catalog__footer-button buy-button">BUY</button>
-
-						</div>
-
-					</div>
-
-					<div class="catalog__item item-catalog">
-
-						<div class="item-catalog__header">
-							<div class="item-catalog__header-icons">
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card1.svg" alt=""></a>
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card2.svg" alt=""></a>
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card3.svg" alt=""></a>
-							</div>
-							<div class="item-catalog__header-heading">
-								R1915
-							</div>
-						</div>
-
-						<div class="item-catalog__image">
-							<img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-item1.jpg" alt="Sxema">
-						</div>
-
-						<div class="item-catalog__footer">
-							<a href="#">Show more</a>
-							<button type="submit" class="item-catalog__footer-button buy-button">BUY</button>
-
-						</div>
-
-					</div>
-
-					<div class="catalog__item item-catalog">
-
-						<div class="item-catalog__header">
-							<div class="item-catalog__header-icons">
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card1.svg" alt=""></a>
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card2.svg" alt=""></a>
-								<a href="#"><img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-card3.svg" alt=""></a>
-							</div>
-							<div class="item-catalog__header-heading">
-								R1915
-							</div>
-						</div>
-
-						<div class="item-catalog__image">
-							<img src="<?=get_template_directory_uri();?>/assets/img/catalog/catalog-item1.jpg" alt="Sxema">
-						</div>
-
-						<div class="item-catalog__footer">
-							<a href="#">Show more</a>
-							<button type="submit" class="item-catalog__footer-button buy-button">BUY</button>
-
-						</div>
-
-					</div>
 
 					<div class="catalog__item item-catalog">
 
