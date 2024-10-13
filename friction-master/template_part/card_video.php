91f66a8cdcd5cc9c1151a3718ddfd12ac472f0e6
Konstantin Baranchuk <kos5anchik@gmail.com>
2024-10-13 12:46:34 +0300

    fix: video on product page playing with full sound (issue#32)

--- friction-master/template_part/card_video.php
+++ friction-master/template_part/card_video.php
@@ -2,7 +2,12 @@
 	<div class="card-video__container">
 		<div class="card-video__wrapper">
 			<div class="card-video__video">
-				<iframe id="video" width="100%" height="100%" src="<?php the_field('video_file'); ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				<video id="video" muted autoplay loop>
+					<?php if(get_field('video_file')) { ?> 
+						<source src="<?php the_field('video_file'); ?>" type="video/mp4" />
+					<?php } ?>
+					<source src="<?php the_field('video_url'); ?>"></source>
+				</video>
 			</div>
 			<div class="card-video__text">
 				<?php the_field('video_url'); ?>
